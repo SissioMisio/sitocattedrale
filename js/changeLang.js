dfaa3e1d6516ac15099a23eb9411b791b5aceaec
SissioMisio <manderioli25@gmail.com>
2024-05-28 20:03:02 +0200

    main4 (traduzione e ristruttura)

---
+++ js/changeLang.js
@@ -0,0 +1,64 @@
+//viene eseguito al caricamento della pagina
+const userLang = navigator.language;
+
+if (!document.cookie) { //se non esiste un cookie
+    if (userLang.startsWith("it")) { //se la lingua del browser è italiano
+        document.cookie = "eng=false;path=/";
+        setLanguage("it");
+    } else { //altrimenti imposto in inglese
+        document.cookie = "eng=true;path=/";
+        setLanguage("en");
+    }
+} else { //se il cookie esiste
+    if (document.cookie.startsWith("eng=true")) { //se indica che è stato precedentemente impostato l'inglese
+        setLanguage("en"); //imposto l'inglese
+    } else { //altrimenti
+        setLanguage("it"); //imposto l'italiano
+    }
+}
+
+// ***** //
+
+async function setLanguage(lang) {
+    const request = new Request("/config/lang.json");
+    const response = await fetch(request);
+    const text = await response.text();
+
+    const traduzioni = JSON.parse(text);
+
+    if (lang === "en") {
+        document.cookie = "eng=true;path=/";
+    } else {
+        document.cookie = "eng=false;path=/";
+    }
+
+    for (let elementsByTagNameElement of document.getElementsByTagName("cambioLingua")) { //per ogni elemento <cambioLingua>
+        const idEl = elementsByTagNameElement.id; //prendi l'id dell'elemento
+
+        if (idEl === "main_lingua") { //se è il menu
+            if(lang === "en") {
+                elementsByTagNameElement.innerHTML = "<img src=\"/images/menu/engflag.png\">English"; //imposta il fatto che si veda la lingua inglese
+            } else {
+                elementsByTagNameElement.innerHTML = "<img src=\"/images/menu/itaflag.png\">Italiano"; //imposta il fatto che si veda la lingua italiana
+            }
+        } else {
+            if(lang === "it") {//esattamente la stessa cosa del menu ma al contrario per il dropdown
+                elementsByTagNameElement.innerHTML = "<a onclick=\"setLanguage('en')\"><img src=\"/images/menu/engflag.png\">English</a>";
+            } else {
+                elementsByTagNameElement.innerHTML = "<a onclick=\"setLanguage('it')\"><img src=\"/images/menu/itaflag.png\">Italiano</a>";
+            }
+        }
+    }
+
+    for (let elementsByTagNameElement of document.getElementsByTagName("traduzione")) { //per ogni elemento <traduzione>
+        const idEl = elementsByTagNameElement.id; //prendi l'id dell'elemento
+
+        let testo = traduzioni[idEl][lang]; //trova il testo tradotto dal json
+
+        if (idEl.startsWith("titolo_")) { //se è il titolo concateno anche il tag del titolo
+            testo = "<title>" + testo + "</title>";
+        }
+
+        elementsByTagNameElement.innerHTML = testo; //imposto il testo finale per quell'elemento
+    }
+}
