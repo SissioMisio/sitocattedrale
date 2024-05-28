46e540d111f1df5d05bca73b541dc5ba40100870
SissioMisio <manderioli25@gmail.com>
2024-05-28 16:25:01 +0200

    main4

---
+++ js/menujs.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 20) { // appena scendo di poco cambia subito il colore del background del menu
+            document.getElementById('cssCambioMenu').href='cambioMenu.css'; //imposto href dell'elemento cssMenu con cambioMenu.css
+        } else {
+            document.getElementById('cssCambioMenu').href='preCambioMenu.css'; //imposto href dell'elemento cssMenu con preCambioMenu.css
+        }
+    });
+});
