af9e27700a3f74d08fde56f717f7aa8109455ce4
SissioMisio <manderioli25@gmail.com>
2024-06-04 19:57:57 +0200

    fix menu

--- js/utils.js
+++ js/utils.js
@@ -36,6 +36,18 @@ if(!Esiste) {
     alert("ERRORE: Impossibile trovare la pagina principale! (sicuro di non aver cancellato \"_MAIN\"?)");
 }
 
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 20) { // appena scendo di poco cambia subito il colore del background del menu
+            document.getElementById('cssCambioMenu').href= rootDir + "/css/cambioMenu.css"; //imposto href dell'elemento cssMenu con cambioMenu.css
+        } else {
+            document.getElementById('cssCambioMenu').href= rootDir + "/css/preCambioMenu.css"; //imposto href dell'elemento cssMenu con preCambioMenu.css
+        }
+    });
+});
+
+
 const path = rootDir + "/config/lang.json";
 const userLang = navigator.language;
 
